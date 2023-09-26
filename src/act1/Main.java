72248cd5311bb2d194c696fca4c2ba9e5cfe8c11
Benny <peptoni0345@gmail.com>
2023-09-26 18:22:56 +0200

    Completed First Activity

---
+++ src/act1/Main.java
@@ -0,0 +1,15 @@
+package act1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Setting the variables
+        int a = 10;
+        int b = 5;
+
+        // Printing the results of different operations
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
