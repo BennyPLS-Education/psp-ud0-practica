7f7b4bd9b75bbcda07280946789a3ba0273ee929
Benny <peptoni0345@gmail.com>
2023-09-26 18:38:15 +0200

    Removed Comments & Added Statement

--- src/act1/Main.java
+++ src/act1/Main.java
@@ -1,12 +1,18 @@
 package act1;
 
+/**
+ * <h2> Exercici 1
+ * <p>
+ * A aquest programa hem de definir dos nombre enters amb el valor que vulgueu
+ * vosaltres. Per pantalla hem de mostrar la suma, la resta la multiplicació i la divisió
+ * d'aquests dos nombres. (Una línia per cada resultat.)
+ */
+
 public class Main {
     public static void main(String[] args) {
-        // Setting the variables
         int a = 10;
         int b = 5;
 
-        // Printing the results of different operations
         System.out.println("Sum   -" + a + b);
         System.out.println("Minus -" + (a - b));
         System.out.println("Mult  -" + a * b);
