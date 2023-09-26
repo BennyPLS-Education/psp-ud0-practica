1b9a8bc60e3bd56e729f50ac97b89c88a6f2b602
Benny <peptoni0345@gmail.com>
2023-09-26 19:03:46 +0200

    Completed Act 3

---
+++ src/act3/Main.java
@@ -0,0 +1,35 @@
+package act3;
+
+
+import java.util.Scanner;
+
+/**
+ * <h2> Exercici 2
+ * <p>
+ * A aquesta activitat hem de dissenyar un programa que calculi el sou d'un
+ * treballador. Ha de demanar quantes hores fa feina al dia, quants de dies al mes fa
+ * feina i a quant cobra les hores. Finalment ha de mostrar el resultat per pantalla.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+
+        int hoursPerDay;
+        int daysPerMonth;
+        double hourlyRate;
+
+        System.out.print("Quantes hores fa feina al dia? ");
+        hoursPerDay = input.nextInt();
+
+        System.out.print("Quants dies al mes fa feina? ");
+        daysPerMonth = input.nextInt();
+
+        System.out.print("A quant cobra les hores? ");
+        hourlyRate = input.nextDouble();
+
+        double salary = hoursPerDay * daysPerMonth * hourlyRate;
+
+        System.out.println("El sou del treballador és " + salary + "€ / mensuals.");
+    }
+}
