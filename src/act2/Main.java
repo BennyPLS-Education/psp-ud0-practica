1587dccd1e3a419419babf9783aef2ad8614cd43
Benny <peptoni0345@gmail.com>
2023-09-26 19:03:42 +0200

    Completed Act 2

---
+++ src/act2/Main.java
@@ -0,0 +1,34 @@
+package act2;
+
+import java.util.Scanner;
+
+/**
+ * <h2> Exercici 2
+ * <p>
+ * A aquesta activitat hem de dissenyar un programa que et faci la mitjana de les notes
+ * d'un alumne, per a això el programa t'ha de sol·licitar el nom de l'alumne i les notes
+ * de les 3 avaluacions per pantalla i mostrar el resultat final.
+ */
+public class Main {
+    public static void main(String[] args) {
+        double[] valuations = new double[3];
+
+        var input = new Scanner(System.in);
+
+        System.out.print("Nom de l'alumne: ");
+        String name = input.nextLine();
+
+        for (int i = 0; i < valuations.length; i++) {
+            System.out.print("Nota de l'avaluació " + (i + 1) + ": ");
+            valuations[i] = input.nextDouble();
+        }
+
+        double average = 0;
+        for (double valuation : valuations) {
+            average += valuation;
+        }
+        average /= valuations.length;
+
+        System.out.println("La nota mitjana de " + name + " és " + average);
+    }
+}
